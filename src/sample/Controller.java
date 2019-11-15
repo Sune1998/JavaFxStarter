bc8fc2f28bf9a5668e4c0a9e0317613d44f477b4
sune <sune1309@edu.easj.dk>
2019-11-15 13:27:33 +0100

    hej

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML
+    Button login;
+    private void hello() {
+        System.out.println("hello");
+
+    }
 
 }
